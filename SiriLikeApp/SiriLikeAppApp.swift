6a691767847808595c81a4f41a80c6f148746386
Vinnicius Pereira <pereiradasilva0@hotmail.com>
2025-01-20 14:19:08 -0300

    Initial Commit

---
+++ SiriLikeApp/SiriLikeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SiriLikeAppApp.swift
+//  SiriLikeApp
+//
+//  Created by Vinnicius Pereira on 20/01/25.
+//
+
+import SwiftUI
+
+@main
+struct SiriLikeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
